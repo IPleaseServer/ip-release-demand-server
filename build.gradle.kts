3eee5bd86147db824bd390bf5540cab6a0a3494b
raul <develop.raul@gmail.com>
2022-07-31 18:02:03 +0900

    [ADD] 개발시에 필요한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	jacoco
 	id("org.springframework.boot") version "2.7.2"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
 	kotlin("jvm") version "1.6.21"
@@ -11,6 +12,12 @@ group = "site.iplease"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
@@ -20,6 +27,53 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+extra["springCloudVersion"] = "2021.0.3"
+
+dependencies {
+	//annotation
+	compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+	compileOnly("jakarta.annotation:jakarta.annotation-api:2.0.0")
+	//kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	//spring boot
+	implementation ("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	implementation ("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	//spring doc
+	implementation ("org.springdoc:springdoc-openapi-webflux-ui:1.6.8")
+	//spring cloud
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
+	//database driver
+	implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+	//jwt
+	implementation ("io.jsonwebtoken:jjwt:0.9.1")
+	//logback
+	implementation("net.logstash.logback:logstash-logback-encoder:7.1.1")
+	//test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation ("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
+	testImplementation ("org.mockito:mockito-inline:2.21.0")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
