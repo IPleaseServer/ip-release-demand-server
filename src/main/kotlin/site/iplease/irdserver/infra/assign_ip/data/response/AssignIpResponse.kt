aac94a2372d6ed3dd750f7080cacf4b39360fa60
raul <develop.raul@gmail.com>
2022-08-05 01:15:16 +0900

    [ADD] HttpAssignIpQueryService구현 및 이에 필요한 컴포넌트 작성
    - WebClientConfig 작성
    - AssignIpResponse 작성
    - DummyAssignIpQueryService 제거

---
+++ src/main/kotlin/site/iplease/irdserver/infra/assign_ip/data/response/AssignIpResponse.kt
@@ -0,0 +1,5 @@
+package site.iplease.irdserver.infra.assign_ip.data.response
+
+import site.iplease.irdserver.infra.assign_ip.data.dto.AssignIpDto
+
+data class AssignIpQueryResponse(val data: AssignIpDto)
