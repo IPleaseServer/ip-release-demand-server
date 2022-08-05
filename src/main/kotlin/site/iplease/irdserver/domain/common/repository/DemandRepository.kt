8ffe7611dd954bd8f8dd64ff6bcac0cfc986f76a
raul <develop.raul@gmail.com>
2022-08-05 19:56:30 +0900

    [ADD] 수락된IP할당해제신청제거 성공 테스트 작성 및 이를 통과하도록 로직 구현
    - AcceptedDemandRemoveStrategyImpl 추가
    - DemandRepository 스팩 추가
    - AcceptedDemandRemoveStrategyImpl.removeAcceptedDemand() 메서드 로직 구현

--- src/main/kotlin/site/iplease/irdserver/domain/common/repository/DemandRepository.kt
+++ src/main/kotlin/site/iplease/irdserver/domain/common/repository/DemandRepository.kt
@@ -3,7 +3,9 @@ package site.iplease.irdserver.domain.common.repository
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import reactor.core.publisher.Mono
 import site.iplease.irdserver.domain.common.data.entity.Demand
+import site.iplease.irdserver.domain.common.data.type.DemandStatusType
 
 interface DemandRepository: ReactiveCrudRepository<Demand, Long> {
     fun existsByAssignIpId(assignIpId: Long): Mono<Boolean>
+    fun deleteAllByStatus(status: DemandStatusType): Mono<Void>
 }
