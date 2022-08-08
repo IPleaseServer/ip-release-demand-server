74c88a0816541a09f0008b9ad31bd29b9d75dd40
raul <develop.raul@gmail.com>
2022-08-08 16:47:05 +0900

    [ADD] IP할당해제예약추가정책검증 성공 테스트 작성 및 이를 통과하도록 로직 구현
    - AlreadyReservedAssignIpException 추가
    - ReservePermission 추가
    - ReservePermissionValidator 추가
    - ReserveRepository 스팩추가
    - ReserveValidatorImpl.validate() 메서드 로직 구현

---
+++ src/main/kotlin/site/iplease/irdserver/domain/reserve/util/ReserveValidatorImpl.kt
@@ -0,0 +1,40 @@
+package site.iplease.irdserver.domain.reserve.util
+
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import site.iplease.irdserver.domain.reserve.data.dto.ReserveDto
+import site.iplease.irdserver.domain.reserve.data.type.ReservePermission
+import site.iplease.irdserver.domain.reserve.data.type.ReservePolicyType
+import site.iplease.irdserver.domain.reserve.exception.AlreadyReservedAssignIpException
+import site.iplease.irdserver.domain.reserve.repository.ReserveRepository
+import site.iplease.irdserver.global.common.exception.UnknownAssignIpException
+import site.iplease.irdserver.infra.assign_ip.service.AssignIpQueryService
+
+class ReserveValidatorImpl(
+    private val reserveRepository: ReserveRepository,
+    private val reservePermissionValidator: ReservePermissionValidator,
+    private val assignIpQueryService: AssignIpQueryService
+): ReserveValidator {
+    override fun validate(dto: ReserveDto, type: ReservePolicyType): Mono<Unit> =
+        when(type) {
+            ReservePolicyType.RESERVE_CREATE ->
+                isNotExistsByAssignIpId(dto.assignIpId)
+                    .flatMap { isAssignIpExists(dto.assignIpId) }
+                    .flatMap { hasPermission(ReservePermission.CREATE, assignIpId =  dto.assignIpId, issuerId = dto.issuerId) }
+        }
+
+    private fun isNotExistsByAssignIpId(assignIpId: Long): Mono<Unit> =
+        reserveRepository.existsByAssignIpId(assignIpId).flatMap { isExists ->
+            if(isExists) Mono.error(AlreadyReservedAssignIpException("이미 id가 ${assignIpId}인 할당IP에 대한 해제예약이 존재합니다!"))
+            else Unit.toMono()
+        }
+
+    private fun isAssignIpExists(assignIpId: Long): Mono<Unit> =
+        assignIpQueryService.existsById(assignIpId).flatMap { isExists ->
+            if(isExists) Unit.toMono()
+            else Mono.error(UnknownAssignIpException("할당IP id가 ${assignIpId}인 할당IP가 존재하지 않습니다!"))
+        }
+
+    private fun hasPermission(permission: ReservePermission, assignIpId: Long, issuerId: Long): Mono<Unit> =
+        reservePermissionValidator.validate(permission, assignIpId = assignIpId, issuerId = issuerId)
+}
