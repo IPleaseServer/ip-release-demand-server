aac5c32bdd193bb06d188d9dd6b8c1c4b022b14d
raul <develop.raul@gmail.com>
2022-08-08 15:47:11 +0900

    [ADD] ReserveDto 프로퍼티 작성

--- src/main/kotlin/site/iplease/irdserver/domain/reserve/data/dto/ReserveDto.kt
+++ src/main/kotlin/site/iplease/irdserver/domain/reserve/data/dto/ReserveDto.kt
@@ -1,5 +1,10 @@
 package site.iplease.irdserver.domain.reserve.data.dto
 
-class ReserveDto {
+import java.time.LocalDate
 
-}
+data class ReserveDto(
+    val id: Long,
+    val assignIpId: Long,
+    val issuerId: Long,
+    val releaseAt: LocalDate
+)
