c8a6af63a224fce85bc51c289e07d94c0d90d0f8
raul <develop.raul@gmail.com>
2022-08-22 20:53:49 +0900

    [ADD] ReleaseDemandQueryServiceImpl 구현체 작성 및 이에 필요한 수정사항 적용
    - DemandRepository 스팩 추가

--- src/main/kotlin/site/iplease/irdserver/domain/demand/repository/DemandRepository.kt
+++ src/main/kotlin/site/iplease/irdserver/domain/demand/repository/DemandRepository.kt
@@ -1,11 +1,15 @@
 package site.iplease.irdserver.domain.demand.repository
 
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import site.iplease.irdserver.domain.demand.data.entity.Demand
 import site.iplease.irdserver.domain.demand.data.type.DemandStatusType
 
 interface DemandRepository: ReactiveCrudRepository<Demand, Long> {
+    fun findBy(page: Pageable): Flux<Demand>//https://stackoverflow.com/questions/58874827/spring-data-r2dbc-and-pagination
+    fun findByIssuerId(page: Pageable, issuerId: Long): Flux<Demand>
     fun existsByAssignIpId(assignIpId: Long): Mono<Boolean>
     fun deleteAllByStatus(status: DemandStatusType): Mono<Void>
 }
